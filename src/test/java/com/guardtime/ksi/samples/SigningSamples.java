4a42fdfba58183a39d4ebf82c1b55e15d264f6a2
andreas.sisask <andreas.sisask@guardtime.com>
2016-03-08 12:06:00 +0200

    Added samples for signing byte array and input stream.

--- src/test/java/com/guardtime/ksi/samples/SigningSamples.java
+++ src/test/java/com/guardtime/ksi/samples/SigningSamples.java
@@ -1,9 +1,11 @@
 package com.guardtime.ksi.samples;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,6 +13,8 @@ import org.junit.Test;
 
 import com.guardtime.ksi.KSI;
 import com.guardtime.ksi.exceptions.KSIException;
+import com.guardtime.ksi.hashing.DataHasher;
+import com.guardtime.ksi.hashing.HashAlgorithm;
 import com.guardtime.ksi.unisignature.KSISignature;
 
 public class SigningSamples extends KsiSamples {
@@ -26,7 +30,7 @@ public class SigningSamples extends KsiSamples {
     }
 
     /**
-     * Creates a sample file and then signs it.
+     * Creates a sample file, then signs it and stores the signature in a file.
      */
     @Test
     public void createAndSignSampleFile() throws IOException, KSIException {
@@ -38,7 +42,7 @@ public class SigningSamples extends KsiSamples {
         writer.println("Sample file, generated for signing!");
         writer.close();
 
-        // Sign it
+        // Sign it, the hash of the document is computed implicitly by the sign method
         KSISignature signature = ksi.sign(fileToSign);
 
         // Persist signature to file
@@ -46,4 +50,49 @@ public class SigningSamples extends KsiSamples {
         signature.writeTo(fileOutputStream);
         fileOutputStream.close();
     }
+
+    /**
+     * Sign a byte array, in this example created from a simple line of text.
+     */
+    @Test
+    public void signSampleByteArray() throws IOException, KSIException {
+        KSI ksi = getKsi();
+
+        // Whenever signing text data, make sure you control and know what the character set
+        // (encoding) was otherwise you may have trouble in the verification later.
+        byte[] document = "This is my document".getBytes(Charset.forName("UTF-8"));
+
+        // Sign it, the hash of the document is computed implicitly by the sign method
+        @SuppressWarnings("unused")
+        KSISignature signature = ksi.sign(document);
+
+        // Persist signature to file
+        // signature.writeTo(...);
+    }
+
+    /**
+     * Compute the hash of the signed document first using an input stream and then provide the hash
+     * to the signing method.
+     */
+    @Test
+    public void signHashDirectly() throws IOException, KSIException {
+        KSI ksi = getKsi();
+
+        // Compute the hash first, use the input stream to provide the data to save memory for
+        // hashing very large documents
+        // In this example we simply use an input stream from an array of bytes but in practice it
+        // could be file input stream from a very large file (several GB)
+        DataHasher dh = new DataHasher(HashAlgorithm.SHA2_256);
+        dh.addData(new ByteArrayInputStream("Imagine this is a large file".getBytes(Charset.forName("UTF-8"))));
+
+        // Provide the signing method with the computed hash instead of document itself
+        @SuppressWarnings("unused")
+        KSISignature signature = ksi.sign(dh.getHash());
+
+        // Persist signature to file
+        // signature.writeTo(...);
+    }
+
+
+
 }
